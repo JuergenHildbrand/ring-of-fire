494aafc488309e3f18a17cc44939a8ef5d0a8a1b
JuergenHildbrand <jhildbrand@pm.me>
2022-10-05 10:43:58 +0200

    secound commit

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+
+  takeCardAnimation = false;
+  currentCard: string = '';
+  game: Game;
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+  }
+
+  takeCard() {
+    if (!this.takeCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.takeCardAnimation = true;
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard)
+        this.takeCardAnimation = false;
+      }, 1000);
+    }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent, {
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+  }
+
+
+}
---
+++ src/models/game.ts
@@ -0,0 +1,48 @@
+export class Game {
+    public players: string[] = ['hans', 'peter', 'fredi'];
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public currenPlayer: number = 0;
+
+    constructor() {
+        for (let i = 1; i < 14; i++) {
+            this.stack.push('ace_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+            this.stack.push('hearts_' + i);
+        }   
+        shuffle(this.stack);
+    }
+
+    // public toJson() {
+    //     return {
+    //         playerNames: this.playerNames,
+    //         stack: this.stack,
+    //         playedCards: this.playedCards,
+    //         takeCardAnimation: this.takeCardAnimation,
+    //         playerImgs: this.playerImgs,
+    //         currentPlayer: this.currentPlayer,
+    //         currentCard: this.currentCard,
+    //         addPlayer: this.addPlayer,
+    //         portrait: this.portrait
+    //     }
+    // }
+}
+
+function shuffle(array) {
+    let currentIndex = array.length, randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+}
